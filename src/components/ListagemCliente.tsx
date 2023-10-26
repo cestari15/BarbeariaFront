6a4d28c8be2bff8b19a1c757cd34b1845d35000b
cestari15 <rafacjacobs1502@gmail.com>
2023-10-26 14:58:05 -0300

    Commit Geral

--- src/components/ListagemCliente.tsx
+++ src/components/ListagemCliente.tsx
@@ -3,147 +3,147 @@ import React, { Component, useState, ChangeEvent, FormEvent, useEffect } from 'r
 
 
 import styles from "../App.module.css";
-
 import { CLienteCadastroInterface } from '../interfaces/ClienteCadastroInterface';
 
 const ListagemClientes = () => {
 
 
-    console.log(Response)
+
     const [cliente, setCliente] = useState<CLienteCadastroInterface[]>([]);
-    const [pesquisa, setPesquisa] = useState<string>('');
     const [error, setError] = useState("");
+    const [pesquisa, setPesquisa] = useState<string>('');
 
 
     const handleState = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.name === "pesquisa") {
             setPesquisa(e.target.value);
         }
-        const buscar = (e: FormEvent) => {
-            e.preventDefault();
+    }
+    const buscar = (e: FormEvent) => {
+        e.preventDefault();
 
-            async function fetchData() {
-                try {
-                    const response = await axios.post('http://127.0.0.1:8000/api/cliente/nome',
-                        { nome: pesquisa },
-                        {
-                            headers: {
-                                "Accept": "application/json",
-                                "Content-Type": "application/json"
-                            }
+        async function fetchData() {
+            try {
+                const response = await axios.post('http://127.0.0.1:8000/api/cliente/nome',
+                    { nome: pesquisa },
+                    {
+                        headers: {
+                            "Accept": "application/json",
+                            "Content-Type": "application/json"
                         }
-                    ).then(function (response) {
-                        if (true == response.data.status) {
-                            setCliente(response.data.data)
-                        }
-                    }).catch(function (error) {
-                        console.log(error)
-                    });
+                    }
+                ).then(function (response) {
+                    if (true == response.data.status) {
+                        setCliente(response.data.data)
+                    }
+                }).catch(function (error) {
+                    console.log(error)
+                });
 
-                } catch (error) {
-                    console.log(error);
-                }
+            } catch (error) {
+                console.log(error);
             }
-            fetchData();
+        }
+        fetchData();
+    }
+
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/api/cliente/retornarTodos');
+                setCliente(response.data.data);
+
+
+            } catch (error) {
+                setError("Ocorreu um erro");
+                console.log(error);
+            }
         }
 
+        fetchData();
+    }, []);
 
-        useEffect(() => {
-            async function fetchData() {
-                try {
-                    const response = await axios.get('http://127.0.0.1:8000/api/cliente/retornarTodos');
-                    setCliente(response.data.data);
+    return (
+        <div>
 
+            <div className='col-md mb-3'>
+                <div className='card'>
+                    <div className='card-body'>
+                        <h5 className='card-title'>
+                            Pesquisar
+                        </h5>
+                        <form onSubmit={buscar} className='row'>
+                            <div className='col-10'>
+                                <input type="text" name='pesquisa' className='form-control'
+                                    onChange={handleState} />
 
-                } catch (error) {
-                    setError("Ocorreu um erro");
-                    console.log(error);
-                }
-            }
+                            </div>
+                            <div className='col-1'>
+                                <button type='submit' className='btn btn-success'>Pesquisar</button>
+                            </div>
 
-            fetchData();
-        }, []);
+                        </form>
+                    </div>
+                </div>
+            </div>
 
-        return (
-            <div>
-
-                <div className='col-md mb-3'>
+            <main className={styles.main}>
+                <div className='container'>
                     <div className='card'>
                         <div className='card-body'>
-                            <h5 className='card-title'>
-                                Pesquisar
-                            </h5>
-                            <form onSubmit={buscar} className='row'>
-                                <div className='col-10'>
-                                    <input type="text" name='pesquisa' className='form-control'
-                                        onChange={handleState} />
-
-                                </div>
-                                <div className='col-1'>
-                                    <button type='submit' className='btn btn-success'>Pesquisar</button>
-                                </div>
-
-                            </form>
+                            <h5 className='card-title'> Listagem de Clientes</h5>
+                            <table className='table table-hover'>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Nome</th>
+                                        <th>Celular</th>
+                                        <th>E-mail</th>
+                                        <th>CPF</th>
+                                        <th>Data de Nascimento</th>
+                                        <th>Cidade</th>
+                                        <th>Pais</th>
+                                        <th>Rua</th>
+                                        <th>Numero</th>
+                                        <th>Bairro</th>
+                                        <th>CEP</th>
+                                        <th>Estado</th>
+                                        <th>Complemento</th>
+                                        <th>Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {cliente.map(cliente => (
+                                        <tr key={cliente.id}>
+                                            <td>{cliente.id}</td>
+                                            <td>{cliente.nome}</td>
+                                            <td>{cliente.celular}</td>
+                                            <td>{cliente.email}</td>
+                                            <td>{cliente.cpf}</td>
+                                            <td>{cliente.dataNascimento}</td>
+                                            <td>{cliente.cidade}</td>
+                                            <td>{cliente.pais}</td>
+                                            <td>{cliente.rua}</td>
+                                            <td>{cliente.numero}</td>
+                                            <td>{cliente.bairro}</td>
+                                            <td>{cliente.cep}</td>
+                                            <td>{cliente.estado}</td>
+                                            <td>{cliente.complemento}</td>
+                                            <td>
+                                                <a href="#" className='btn btn-primary btn-sm'>Editar</a>
+                                                <a href="#" className='btn btn-danger btn-sm'>Excluir</a>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
-
-                <main className={styles.main}>
-                    <div className='container'>
-                        <div className='card'>
-                            <div className='card-body'>
-                                <h5 className='card-title'> Listagem de Clientes</h5>
-                                <table className='table table-hover'>
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Nome</th>
-                                            <th>Celular</th>
-                                            <th>E-mail</th>
-                                            <th>CPF</th>
-                                            <th>Data de Nascimento</th>
-                                            <th>Cidade</th>
-                                            <th>Pais</th>
-                                            <th>Rua</th>
-                                            <th>Numero</th>
-                                            <th>Bairro</th>
-                                            <th>CEP</th>
-                                            <th>Estado</th>
-                                            <th>Complemento</th>
-                                            <th>Ações</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {cliente.map(cliente => (
-                                            <tr key={cliente.id}>
-                                                <td>{cliente.id}</td>
-                                                <td>{cliente.nome}</td>
-                                                <td>{cliente.celular}</td>
-                                                <td>{cliente.email}</td>
-                                                <td>{cliente.cpf}</td>
-                                                <td>{cliente.dataNascimento}</td>
-                                                <td>{cliente.cidade}</td>
-                                                <td>{cliente.pais}</td>
-                                                <td>{cliente.rua}</td>
-                                                <td>{cliente.numero}</td>
-                                                <td>{cliente.bairro}</td>
-                                                <td>{cliente.cep}</td>
-                                                <td>{cliente.estado}</td>
-                                                <td>{cliente.complemento}</td>
-                                                <td>
-                                                    <a href="#" className='btn btn-primary btn-sm'>Editar</a>
-                                                    <a href="#" className='btn btn-danger btn-sm'>Excluir</a>
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-            </div>
-        );
-    }
+            </main>
+        </div>
+    );
 }
+
 export default ListagemClientes;
